b08a6ba039b7ec11882595ada50543817a19c955
Karl Cronburg <karl.cronburg@gmail.com>
2016-11-23 11:43:48 -0500

    empty ATN test case main file along with a blank template for creating new test cases

---
+++ Test/ATN.hs
@@ -0,0 +1,17 @@
+module Main where
+import Text.Allstar.ATN
+
+import System.IO.Unsafe (unsafePerformIO)           
+import Data.Monoid                                  
+import Test.Framework                               
+import Test.Framework.Providers.HUnit               
+import Test.Framework.Providers.QuickCheck2         
+import Test.HUnit                                   
+import Test.QuickCheck (Property, quickCheck, (==>))
+import qualified Test.QuickCheck.Monadic as TQM     
+
+main :: IO ()
+main = defaultMainWithOpts
+  [
+  ] mempty
+
---
+++ Test/TestTemplate.hs
@@ -0,0 +1,18 @@
+module Main where
+-- Allstar imports go here, e.g.:
+-- import Text.Allstar.ATN (..)
+
+import System.IO.Unsafe (unsafePerformIO)           
+import Data.Monoid                                  
+import Test.Framework                               
+import Test.Framework.Providers.HUnit               
+import Test.Framework.Providers.QuickCheck2         
+import Test.HUnit                                   
+import Test.QuickCheck (Property, quickCheck, (==>))
+import qualified Test.QuickCheck.Monadic as TQM     
+
+main :: IO ()
+main = defaultMainWithOpts
+	[
+	] mempty
+
