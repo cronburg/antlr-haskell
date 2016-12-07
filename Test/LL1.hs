73f017ef5267bd803166830de429c5a430c6303d
Karl Cronburg <karl.cronburg@gmail.com>
2016-12-07 18:17:29 -0500

    first set appears to be working for simple expression grammar given in dragon book

--- Test/LL1.hs
+++ Test/LL1.hs
@@ -4,6 +4,7 @@ import Text.ANTLR.Allstar.Grammar
 import Text.ANTLR.LL1
 
 import Data.Set (fromList, union, empty)
+import qualified Data.Set as S
 
 import System.IO.Unsafe (unsafePerformIO)           
 import Data.Monoid                                  
@@ -22,6 +23,11 @@ firstF = first grm (NT "F") @?= fromList [Term "(", Term "id"]
 
 noEps = first grm (NT "E") @?= fromList [Term "(", Term "id"]
 
+firstT' =
+  first grm (NT "T'")
+  @?=
+  fromList [Term "*", Eps']
+
 foldEpsTest = foldWhileEpsilon union empty
   [ fromList [Term "(", Term "id"]
   , fromList [Term ")"]
@@ -29,11 +35,33 @@ foldEpsTest = foldWhileEpsilon union empty
   @?=
   fromList [Term "(", Term "id"]
 
+firstAll =
+  ( S.map ((\nt -> (nt, first grm nt)) . NT) (ns grm)
+    `union`
+    S.map ((\t  -> (t,  first grm t))  . T)  (ts grm)
+  )
+  @?=
+  fromList
+    [ (NT "E",  fromList [Term "(", Term "id"])
+    , (NT "E'", fromList [Term "+", Eps'])
+    , (NT "F",  fromList [Term "(", Term "id"])
+    , (NT "T",  fromList [Term "(", Term "id"])
+    , (NT "T'", fromList [Term "*", Eps'])
+    , (T "(",   fromList [Term "("])
+    , (T ")",   fromList [Term ")"])
+    , (T "*",   fromList [Term "*"])
+    , (T "+",   fromList [Term "+"])
+    , (T "id",  fromList [Term "id"])
+    ]       
+                                                                                         
+
 main :: IO ()
 main = defaultMainWithOpts
   [ testCase "fold_epsilon" foldEpsTest
-  --, testCase "termination" termination
-  --, testCase "no_epsilon" noEps
+  , testCase "termination" termination
+  , testCase "no_epsilon" noEps
   , testCase "firstF" firstF
+  , testCase "firstT'" firstT'
+  , testCase "firstAll" firstAll
   ] mempty
 
