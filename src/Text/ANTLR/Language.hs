222f603c8c4a7032398593c5b1e13905d8872efd
Karl Cronburg <karl.cronburg@gmail.com>
2016-12-27 18:16:07 -0500

    useless, inefficient, terrible language module for (finite) languages

---
+++ src/Text/ANTLR/Language.hs
@@ -0,0 +1,31 @@
+module Language.ANTLR4.Language
+  ( Alphabet(..), ascii, isASCII
+  ) where
+import Data.Set (Set(..))
+import qualified Data.Set as Set
+
+import Data.Char
+
+type Alphabet a = Set a
+
+ascii :: Alphabet Char
+ascii     = Set.fromList $ map chr [0 .. 127]
+
+isASCII :: Char -> Bool
+isASCII c = ord c < 127
+
+type Word a = [a]
+
+type Language a = Set (Word a) 
+
+union  = Set.union
+
+concat :: Language a -> Language a -> Language a
+concat a b = Set.fromList
+  [ s ++ t
+  | s <- Set.toList a
+  , t <- Set.toList b
+  ]
+
+kleene = undefined
+
