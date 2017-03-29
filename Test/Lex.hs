df968a273f76522fabf5b0c05cc263ed65633a5c
Karl Cronburg <karl.cronburg@gmail.com>
2017-03-29 11:37:22 -0400

    tests for validity of an NFA

--- Test/Lex.hs
+++ Test/Lex.hs
@@ -12,8 +12,35 @@ import Test.HUnit
 import Test.QuickCheck (Property, quickCheck, (==>))
 import qualified Test.QuickCheck.Monadic as TQM     
 
+import Data.Set (fromList, Set(..))
+
+fL :: Ord a => [a] -> Set a
+fL = fromList
+
+nfa0 :: NFA Char Int
+nfa0 = NFA
+  { _S = fL [0, 1, 2, 3]
+  , _Σ = fL "ab"
+  , s0 = 0
+  , _F = fL [3]
+  , _Δ = fL
+    [ (0, 'a', fL [0,1])
+    , (0, 'b', fL [0])
+    , (1, 'b', fL [2])
+    , (2, 'b', fL [3])
+    ]
+  }
+
+testValid0 =
+  True
+  @?=
+  (  validStartState nfa0
+  && validFinalStates nfa0
+  && validTransitions nfa0
+  )
+
 main :: IO ()
 main = defaultMainWithOpts
-  [
+  [ testCase "testValid0" testValid0
   ] mempty
 
--- src/Text/ANTLR/Lex.hs
+++ src/Text/ANTLR/Lex.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE FlexibleInstances, InstanceSigs, DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances, InstanceSigs, DeriveDataTypeable
+    , ScopedTypeVariables #-}
 module Text.ANTLR.Lex where
-import Text.ANTLR.Allstar.Grammar
+import qualified Text.ANTLR.Allstar.Grammar as G
 import Text.ANTLR.AST (AST(..))
 
 import Text.ANTLR.LR1 (lr1Parse)
 import Text.ANTLR.Parser
 
-import Data.Set (fromList, member, (\\), empty)
+import Data.Set (fromList, member, (\\), empty, Set(..), toList)
 import Data.Data (Data(..), Typeable(..), toConstr, dataTypeConstrs, dataTypeOf)
 
 -- Token with name (n) and value (v).
@@ -23,6 +24,45 @@ tokenName (Token x) = fst x
 tokenValue :: Token n v -> v
 tokenValue (Token x) = snd x
 
+data Regex s =
+    Epsilon
+  | Symbol   s
+  | Union     (Regex s) (Regex s)
+  | Concat    (Regex s) (Regex s)
+  | Kleene    (Regex s)
+  | PosClos   (Regex s)
+  | Question  (Regex s)
+  | CharClass [s] -- TODO: Set s, and ranges of characters
+-- TODO: Lex regexs (e.g. complement sets, escape chars, ...)
+
+data NFA s i = NFA
+  { _S :: Set (State i)        -- Finite set of states.
+  , _Σ :: Set s                -- Input (NFA edge) alphabet
+  , _Δ :: Set (Transition s i) -- Transition function
+  , s0 :: State i              -- Start state
+  , _F :: Set (State i)        -- Accepting states
+  }
+
+type Transition s i = (State i, Edge s, Set (State i))
+
+type State i = i
+type Edge  s = s
+
+data Result = Accept | Reject
+
+validStartState nfa = s0 nfa `member` _S nfa
+
+validFinalStates nfa = and [s `member` _S nfa | s <- toList $ _F nfa]
+
+validTransitions :: forall s i. (Ord (State i)) => NFA s i -> Bool
+validTransitions nfa = let
+    vT :: [Transition s i] -> Bool
+    vT [] = True
+    vT ((s1, e, s2):rest) =
+         s1 `member` _S nfa
+      && (and [s2' `member` _S nfa | s2' <- toList s2])
+      && vT rest
+  in vT $ (toList . _Δ) nfa
 
 {-
 regexGrammar = defaultGrammar
