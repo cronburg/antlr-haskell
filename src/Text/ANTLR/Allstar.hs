03f593dcd04dc88c9e3b3d366e3755729d74da45
Karl Cronburg <karl.cronburg@gmail.com>
2017-08-25 09:51:46 -0400

    drop in for what needs to be implemented to use allstar's parse function

--- src/Text/ANTLR/Allstar.hs
+++ src/Text/ANTLR/Allstar.hs
@@ -8,11 +8,15 @@ module Text.ANTLR.Allstar
 import ParserGenerator.AllStar
 
 import qualified Text.ANTLR.Parser as P
+import qualified Text.ANTLR.Grammar as G
 
 -- | Go from an Allstar AST to the AST type used internally in this package
 fromAllstarAST :: AST nts t -> P.AST nts t
 fromAllstarAST (Node nt asts) = P.AST nt [] (map fromAllstarAST asts)
 fromAllstarAST (Leaf tok)     = P.Leaf tok
 
-
+-- | Go from an antlr-haskell Grammar to an Allstar ATNEnv
+--   TODO: Handle predicate and mutator state during the conversion
+atnOf :: G.Grammar s nt t -> ATN nt t
+atnOf = undefined
 
