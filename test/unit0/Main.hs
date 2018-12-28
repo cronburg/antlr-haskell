1b71cdd75d13e85e54e405077cbbf78bf4631e3c
Karl Cronburg <karl.cronburg@gmail.com>
2018-12-28 15:36:46 -0500

    (non-malformed) test case for #33

---
+++ test/unit0/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, TypeFamilies, QuasiQuotes
+    , DataKinds, ScopedTypeVariables, OverloadedStrings, TypeSynonymInstances
+    , FlexibleInstances, UndecidableInstances, FlexibleContexts, TemplateHaskell
+    , DeriveDataTypeable #-}
+module Main where
+import Language.ANTLR4
+
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Monoid
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
+import Test.HUnit
+import Test.QuickCheck (Property, quickCheck, (==>))
+import qualified Test.QuickCheck.Monadic as TQM
+
+import Language.Haskell.TH.Syntax (lift)
+import qualified Text.ANTLR.LR as LR
+
+import Star0
+
+main :: IO ()
+main = defaultMainWithOpts
+  [ testCase "test_star0" test_star0
+  ] mempty
+
---
+++ test/unit0/Star0.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, TypeFamilies, QuasiQuotes
+    , DataKinds, ScopedTypeVariables, OverloadedStrings, TypeSynonymInstances
+    , FlexibleInstances, UndecidableInstances, FlexibleContexts, TemplateHaskell
+    , DeriveDataTypeable #-}
+module Star0 where
+import Language.ANTLR4
+
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Monoid
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
+import Test.HUnit
+import Test.QuickCheck (Property, quickCheck, (==>))
+import qualified Test.QuickCheck.Monadic as TQM
+
+import Language.Haskell.TH.Syntax (lift)
+import qualified Text.ANTLR.LR as LR
+
+[g4|
+  grammar Star0;
+
+  words   : page* -> ${\ps -> ps} ;
+
+  page    : Page ;
+
+  Page : 'page' -> String ;
+
+  WS      : [ \t\n\r\f\v]+     -> String;
+|]
+
+test_star0 = case glrParse (== T_WS) "page page" of
+  (ResultAccept ast) -> ast2words ast @?= ["page", "page"]
+  rest -> assertFailure $ "Did not parse: " ++ pshow' rest
+
